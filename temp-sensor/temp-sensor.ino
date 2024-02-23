4ed9de1248351e1905928c3fbaddbeeb9183f952
ryanozy <2837320O@student.gla.ac.uk>
2024-02-23 10:24:59 +0800

    Update to use M5StickC-Plus instead

---
+++ temp-sensor/temp-sensor.ino
@@ -0,0 +1,95 @@
+/*
+*******************************************************************************
+* Copyright (c) 2021 by M5Stack
+*                  Equipped with M5StickC-Plus sample source code
+*                             M5StickC-Plus
+* Visit for more information: https://docs.m5stack.com/en/core/m5stickc_plus
+*
+* Describe: MQTT
+* Date: 2021/11/5
+*******************************************************************************
+*/
+#include "M5StickCPlus.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+
+#define DHTPIN 26
+#define DHTTYPE DHT11
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid        = "Pixel_5725A";
+const char* password    = "82208220*Tt";
+const char* mqtt_server = "192.168.243.183";
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE]; 
+int value = 0;
+
+void setupWifi();
+void reConnect();
+
+void setup() {
+    M5.begin();
+    M5.Lcd.setRotation(3);
+    setupWifi();
+    client.setServer(mqtt_server, 1883);
+
+    dht.begin();
+}
+
+void loop() {
+    if (!client.connected()) {
+        reConnect();
+    }
+    client.loop();
+
+    delay(2000);
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    snprintf (msg, MSG_BUFFER_SIZE, "{\"temperature\":%.2f,\"humidity\":%.2f}", t, h);
+    client.publish("temperature-humidity", msg);
+}
+
+void setupWifi() {
+    delay(10);
+    M5.Lcd.printf("Connecting to %s", ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        M5.Lcd.print(".");
+    }
+    M5.Lcd.printf("\nSuccess\n");
+    Serial.println(WiFi.localIP());
+    M5.Lcd.print("IP: ");
+    M5.Lcd.print(WiFi.localIP());
+}
+
+void reConnect() {
+    while (!client.connected()) {
+        M5.Lcd.print("Attempting MQTT connection...");
+        String clientId = "M5Stack-";
+        clientId += String(random(0xffff), HEX);
+        if (client.connect(clientId.c_str())) {
+            M5.Lcd.printf("\nSuccess\n");
+        } else {
+            M5.Lcd.print("failed, rc=");
+            M5.Lcd.print(client.state());
+            M5.Lcd.println("try again in 5 seconds");
+            delay(5000);
+        }
+    }
+}
