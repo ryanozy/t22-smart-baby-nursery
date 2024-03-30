c4d464207a966dd1dfa81b35750cb11b38b0de73
ryanozy <2837320O@student.gla.ac.uk>
2024-03-30 18:59:54 +0800

    Performance Testing Codes

---
+++ ble-test/ble-server/ble-server.ino
@@ -0,0 +1,186 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <M5StickCPlus.h>
+
+// Change to unique BLE server name
+#define bleServerName "CSC2106-BLE#369"
+
+double currentTime = 0.0;
+float vBatt = 5.0; // initial value
+
+int deviceConnected = 0;
+
+#define SERVICE_UUID "01234567-0123-4567-89ab-0123456789ab"
+
+// Temperature Characteristic and Descriptor
+BLECharacteristic imuTemperatureCelsiusCharacteristics("01234567-0123-4567-89ab-0123456789cd", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor imuTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902));
+
+// LED Characteristic and Descriptor
+BLECharacteristic ledCharacteristics("01234567-0123-4567-89ab-0123456789ff", BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor ledDescriptor(BLEUUID((uint16_t)0x2904));
+
+bool setStatus();
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer)
+    {
+        deviceConnected++;
+        Serial.println("MyServerCallbacks::Connected...");
+        BLEDevice::startAdvertising();
+    };
+    void onDisconnect(BLEServer *pServer)
+    {
+        deviceConnected--;
+        Serial.println("MyServerCallbacks::Disconnected...");
+    }
+};
+
+bool setStatus()
+{
+
+    currentTime = millis();
+    // Convert Value to String
+    static char tempStr[20];
+    static char ledStr[10];
+
+    dtostrf(currentTime, 6, 2, tempStr);
+
+    // Set IMU temperature
+    imuTemperatureCelsiusCharacteristics.setValue(tempStr);
+    imuTemperatureCelsiusCharacteristics.notify();
+
+    if (digitalRead(M5_LED) == 0)
+    {
+        strncpy(ledStr, "ON ", sizeof(ledStr));
+    }
+    else
+    {
+        strncpy(ledStr, "OFF", sizeof(ledStr));
+    }
+
+    // Set LED status
+    ledCharacteristics.setValue(ledStr);
+    ledCharacteristics.notify();
+
+    return true;
+}
+
+void printReadings()
+{
+    // Print to LCD
+    // Temperature Celsius
+    M5.Lcd.setCursor(0, 20, 2);
+    M5.Lcd.print("Timestamp = ");
+    M5.Lcd.print(currentTime);
+
+    // display LED status
+    M5.Lcd.setCursor(0, 40, 2);
+    M5.Lcd.print("LED Status = ");
+
+    if (digitalRead(M5_LED) == 0)
+    {
+        M5.Lcd.print("ON ");
+        Serial.println("ON ");
+    }
+    else
+    {
+        M5.Lcd.print("OFF");
+        Serial.println("OFF");
+    }
+}
+
+void setup()
+{
+    // Start serial communication
+    Serial.begin(115200);
+
+    // Start M5StickC
+    M5.begin();
+
+    // Setup IMU
+    M5.IMU.Init();
+
+    // Set LCD display
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0, 2);
+    M5.Lcd.printf("BLE Server", 0);
+
+    // Setup button
+    pinMode(M5_BUTTON_HOME, INPUT);
+    delay(100);
+
+    // Setup LED
+    pinMode(M5_LED, OUTPUT);
+    digitalWrite(M5_LED, 1); // Turn off LED
+
+    // Setup AXP192
+    M5.Axp.begin();
+
+    // Create BLE Server
+    BLEDevice::init(bleServerName);
+
+    // Create BLE Server
+    BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    BLEService *bleService = pServer->createService(SERVICE_UUID);
+
+    // Create BLE Characteristics
+    bleService->addCharacteristic(&imuTemperatureCelsiusCharacteristics);
+    imuTemperatureCelsiusDescriptor.setValue("IMU Temperature(C)");
+    imuTemperatureCelsiusCharacteristics.addDescriptor(&imuTemperatureCelsiusDescriptor);
+
+    // LED
+    bleService->addCharacteristic(&ledCharacteristics);
+    ledDescriptor.setValue("LED");
+    ledCharacteristics.addDescriptor(&ledDescriptor);
+
+    // Start the service
+    bleService->start();
+
+    // Start advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting a client connection to notify...");
+}
+
+void loop()
+{
+    if (deviceConnected >= 1)
+    {
+        std::string value = ledCharacteristics.getValue();
+
+        if (value[0] == 'R')
+        {
+            setStatus();
+            printReadings();
+        }
+        else if (value[0] == 'L')
+        {
+            digitalWrite(M5_LED, !digitalRead(M5_LED));
+            setStatus();
+            printReadings();
+        }
+    }
+
+    // Check button
+    if (digitalRead(M5_BUTTON_HOME) == LOW)
+    {
+        // Toggle LED
+        digitalWrite(M5_LED, !digitalRead(M5_LED));
+
+        // Set all status to BLE
+        setStatus();
+
+        // Print readings
+        printReadings();
+
+        // Wait for button release
+        while (digitalRead(M5_BUTTON_HOME) == LOW)
+            ;
+    }
+}
---
+++ ble-test/led-w-ble/led-w-ble.ino
@@ -0,0 +1,224 @@
+#include "BLEDevice.h"
+#include <M5StickCPlus.h>
+
+// change the BLE Server name to connect to
+#define bleServerName "CSC2106-BLE#369"
+
+/* UUID's of the service, characteristic that we want to read*/
+// BLE Service
+static BLEUUID bleServiceUUID("01234567-0123-4567-89ab-0123456789ab");
+
+static BLEUUID temperatureCharacteristicUUID("01234567-0123-4567-89ab-0123456789cd");
+
+// LED Characteristic
+static BLEUUID ledCharacteristicUUID("01234567-0123-4567-89ab-0123456789ff");
+
+// Flags stating if should begin connecting and if the connection is up
+static boolean doConnect = false;
+static boolean connected = false;
+
+// Address of the peripheral device. Address will be found during scanning...
+static BLEAddress *pServerAddress;
+
+// Characteristicd that we want to read
+static BLERemoteCharacteristic *temperatureCharacteristic;
+static BLERemoteCharacteristic *voltageCharacteristic;
+static BLERemoteCharacteristic *ledCharacteristic;
+
+// Activate notify
+const uint8_t notificationOn[] = {0x1, 0x0};
+const uint8_t notificationOff[] = {0x0, 0x0};
+
+// Variables to store temperature and voltage
+char temperatureStr[20];  
+char ledStr[10]; 
+
+boolean newTemperature = false;
+boolean newVoltage = false;
+boolean newLed = false;
+
+// Connect to the BLE Server that has the name, Service, and Characteristics
+bool connectToServer(BLEAddress pAddress)
+{
+    BLEClient *pClient = BLEDevice::createClient();
+
+    // Connect to the remove BLE Server.
+    pClient->connect(pAddress);
+    Serial.println(" - Connected to server");
+
+    // Obtain a reference to the service we are after in the remote BLE server.
+    BLERemoteService *pRemoteService = pClient->getService(bleServiceUUID);
+    if (pRemoteService == nullptr)
+    {
+        Serial.print("Failed to find our service UUID: ");
+        Serial.println(bleServiceUUID.toString().c_str());
+        return (false);
+    }
+
+    // Obtain a reference to the characteristics in the service of the remote BLE server.
+    temperatureCharacteristic = pRemoteService->getCharacteristic(temperatureCharacteristicUUID);
+    ledCharacteristic = pRemoteService->getCharacteristic(ledCharacteristicUUID);
+
+    if (temperatureCharacteristic == nullptr || ledCharacteristic == nullptr)
+    {
+        Serial.print("Failed to find our characteristic UUID");
+        return false;
+    }
+    Serial.println(" - Found all characteristics");
+
+    // Assign callback functions for the Characteristics
+    temperatureCharacteristic->registerForNotify(temperatureNotifyCallback);
+
+    // Assign callback function for the LED characteristic
+    if (ledCharacteristic->canWrite())
+    {
+        std::string value = ledCharacteristic->readValue();
+        Serial.print("The characteristic value was: ");
+        Serial.println(value.c_str());
+    }
+
+    if (ledCharacteristic->canNotify())
+    {
+        ledCharacteristic->registerForNotify(ledNotifyCallback);
+    }
+
+    return true;
+}
+
+// Callback function that gets called, when another device's advertisement has been received
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+    void onResult(BLEAdvertisedDevice advertisedDevice)
+    {
+        if (advertisedDevice.getName() == bleServerName)
+        {                                                                   // Check if the name of the advertiser matches
+            advertisedDevice.getScan()->stop();                             // Scan can be stopped, we found what we are looking for
+            pServerAddress = new BLEAddress(advertisedDevice.getAddress()); // Address of advertiser is the one we need
+            doConnect = true;                                               // Set indicator, stating that we are ready to connect
+            Serial.println("Device found. Connecting!");
+        }
+        else
+            Serial.print(".");
+    }
+};
+
+// When the BLE Server sends a new temperature reading with the notify property
+static void temperatureNotifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+                                      uint8_t *pData, size_t length, bool isNotify)
+{
+    // store temperature value
+    strncpy(temperatureStr, (char*)pData, length);
+    temperatureStr[length] = '\0';
+    newTemperature = true;
+}
+
+static void ledNotifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic,
+                              uint8_t *pData, size_t length, bool isNotify)
+{
+    // store LED status
+    strncpy(ledStr, (char*)pData, length);
+    ledStr[length] = '\0';
+    newLed = true;
+}
+
+// function that prints the latest sensor readings in the OLED display
+void printReadings()
+{
+    // Temperature Celsius
+    M5.Lcd.setCursor(0, 20, 2);
+    M5.Lcd.print("Timestamp = ");
+    M5.Lcd.print(temperatureStr);
+    M5.Lcd.println("");
+
+    double receivedVal = atof(temperatureStr);
+
+    M5.Lcd.setCursor(0, 40, 2);
+    M5.Lcd.print("Latency = ");
+    M5.Lcd.print(millis()-receivedVal);
+    M5.Lcd.println("");
+
+    // display LED status
+    M5.Lcd.setCursor(0, 60, 2);
+    M5.Lcd.print("LED Status = ");
+    M5.Lcd.print(ledStr);
+    M5.Lcd.println(" ");
+}
+
+void setup()
+{
+    // Start serial communication
+    Serial.begin(115200);
+    Serial.println("Starting BLE Client application...");
+
+    // put your setup code here, to run once:
+    M5.begin();
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0, 2);
+    M5.Lcd.printf("BLE Client", 0);
+
+    // Setup Button
+    pinMode(M5_BUTTON_HOME, INPUT);
+    delay(100);
+
+    // Init BLE device
+    BLEDevice::init("");
+
+    // Retrieve a Scanner and set the callback we want to use to be informed when we
+    // have detected a new device.  Specify that we want active scanning and start the
+    // scan to run for 30 seconds.
+    BLEScan *pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    pBLEScan->setActiveScan(true);
+    pBLEScan->start(30);
+}
+
+void loop()
+{
+    // If the flag "doConnect" is true then we have scanned for and found the desired
+    // BLE Server that we want to connect.  Now we connect to it.  Once we are
+    // connected we set the connected flag to be true.
+    if (doConnect == true)
+    {
+        if (connectToServer(*pServerAddress))
+        {
+            Serial.println("Connected to the BLE Server.");
+
+            // Activate the Notify property of each Characteristic
+            temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t *)notificationOn, 2, true);
+            ledCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2904))->writeValue((uint8_t *)notificationOn, 2, true);
+            connected = true;
+        }
+        else
+        {
+            Serial.println("Failed to connect to the server; Restart device to scan for nearby BLE server again.");
+        }
+        doConnect = false;
+    }
+
+    if (connected)
+    {
+        if (newTemperature || newVoltage || newLed)
+        {
+            printReadings();
+            newTemperature = false;
+            newLed = false;
+        }
+
+        if (digitalRead(M5_BUTTON_HOME) == LOW)
+        {
+            String Reqest = "Read Data";
+            ledCharacteristic->writeValue(Reqest.c_str(), Reqest.length());
+
+            String Request = "Led";
+            ledCharacteristic->writeValue(Request.c_str(), Request.length());
+
+            // Wait for button release
+            while (digitalRead(M5_BUTTON_HOME) == LOW)
+            ;
+        }
+    }
+
+
+    delay(1000); // Delay one second between loops.
+}
